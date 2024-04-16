dcdba11b4882f511e61f9a0f0b95e63024176f2e
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-04-16 11:03:27 -0700

    refactor: create Repository class

--- src/main.zig
+++ src/main.zig
@@ -4,24 +4,32 @@ const git = @cImport(@cInclude("git2.h"));
 pub fn main() !void {
     _ = git.git_libgit2_init();
     var repo: ?*git.git_repository = undefined;
-    const err = git.git_repository_open(&repo, ".");
-    if (err < 0) return;
-    var walker: ?*git.git_revwalk = undefined;
-    _ = git.git_revwalk_new(&walker, repo);
-    _ = git.git_revwalk_push_ref(walker, "HEAD");
-    var oid: git.git_oid = undefined;
-    _ = git.git_revwalk_next(&oid, walker);
-    var commit: ?*git.git_commit = undefined;
-    _ = git.git_commit_lookup(&commit, repo, &oid);
-    std.debug.print("{s}\n", .{git.git_commit_message(commit)});
-    _ = git.git_status_foreach(repo, status_cb, null);
-    _ = git.git_libgit2_shutdown();
+    if (0 < git.git_repository_open(&repo, ".")) return;
+    var rp = Repository{ .repo = repo };
+    try rp.log();
+    try rp.status();
     return;
 }
 
-fn status_cb(path: [*c]const u8, status_flags: c_uint, payload: ?*anyopaque) callconv(.C) c_int {
-    _ = payload;
-    std.debug.print("{s}\n", .{path});
-    std.debug.print("{}\n", .{status_flags});
-    return 0;
-}
+const Repository = struct {
+    repo: ?*git.git_repository,
+    pub fn log(self: Repository) !void {
+        var walker: ?*git.git_revwalk = undefined;
+        _ = git.git_revwalk_new(&walker, self.repo);
+        _ = git.git_revwalk_push_ref(walker, "HEAD");
+        var oid: git.git_oid = undefined;
+        _ = git.git_revwalk_next(&oid, walker);
+        var commit: ?*git.git_commit = undefined;
+        _ = git.git_commit_lookup(&commit, self.repo, &oid);
+        std.debug.print("{s}\n", .{git.git_commit_message(commit)});
+    }
+    pub fn status(self: Repository) !void {
+        _ = git.git_status_foreach(self.repo, status_cb, null);
+    }
+    fn status_cb(path: [*c]const u8, status_flags: c_uint, payload: ?*anyopaque) callconv(.C) c_int {
+        _ = payload;
+        std.debug.print("{s}\n", .{path});
+        std.debug.print("{}\n", .{status_flags});
+        return 0;
+    }
+};
