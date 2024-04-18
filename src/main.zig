439cac5873681f48b743e037ea95f141814ee561
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-04-17 18:54:13 -0700

    refactor: rm oop

--- src/main.zig
+++ src/main.zig
@@ -5,52 +5,53 @@ pub fn main() !void {
     _ = git.git_libgit2_init();
     var repo: ?*git.git_repository = undefined;
     if (0 < git.git_repository_open(&repo, ".")) return;
-    var rp = Repository{ .repo = repo };
-    try rp.log();
-    try rp.status();
-    try rp.branch();
-    try rp.stash();
+    try log(repo);
+    try status(repo);
+    try branch(repo);
+    try stash(repo);
     return;
 }
 
 var COUNT: u8 = 0;
 
-const Repository = struct {
-    repo: ?*git.git_repository,
-    pub fn branch(self: Repository) !void {
-        var ref: ?*git.git_reference = undefined;
-        _ = git.git_repository_head(&ref, self.repo);
-        std.debug.print("{s}\n", .{git.git_reference_shorthand(ref)});
-    }
-    pub fn log(self: Repository) !void {
-        var walker: ?*git.git_revwalk = undefined;
-        _ = git.git_revwalk_new(&walker, self.repo);
-        _ = git.git_revwalk_push_ref(walker, "HEAD");
-        var oid: git.git_oid = undefined;
-        _ = git.git_revwalk_next(&oid, walker);
-        var commit: ?*git.git_commit = undefined;
-        _ = git.git_commit_lookup(&commit, self.repo, &oid);
-        std.debug.print("{s}\n", .{git.git_commit_message(commit)});
-    }
-    pub fn stash(self: Repository) !void {
-        _ = git.git_stash_foreach(self.repo, stash_cb, null);
-        std.debug.print("Stashes: {}\n", .{COUNT});
-    }
-    fn stash_cb(index: usize, message: [*c]const u8, stash_id: [*c]const git.git_oid, payload: ?*anyopaque) callconv(.C) c_int {
-        _ = index;
-        _ = payload;
-        _ = stash_id;
-        _ = message;
-        COUNT += 1;
-        return 0;
-    }
-    pub fn status(self: Repository) !void {
-        _ = git.git_status_foreach(self.repo, status_cb, null);
-    }
-    fn status_cb(path: [*c]const u8, status_flags: c_uint, payload: ?*anyopaque) callconv(.C) c_int {
-        _ = payload;
-        std.debug.print("{s}\n", .{path});
-        std.debug.print("{}\n", .{status_flags});
-        return 0;
-    }
-};
+fn branch(repo: ?*git.git_repository) !void {
+    var ref: ?*git.git_reference = undefined;
+    _ = git.git_repository_head(&ref, repo);
+    std.debug.print("{s}\n", .{git.git_reference_shorthand(ref)});
+}
+
+fn log(repo: ?*git.git_repository) !void {
+    var walker: ?*git.git_revwalk = undefined;
+    _ = git.git_revwalk_new(&walker, repo);
+    _ = git.git_revwalk_push_ref(walker, "HEAD");
+    var oid: git.git_oid = undefined;
+    _ = git.git_revwalk_next(&oid, walker);
+    var commit: ?*git.git_commit = undefined;
+    _ = git.git_commit_lookup(&commit, repo, &oid);
+    std.debug.print("{s}\n", .{git.git_commit_message(commit)});
+}
+
+fn stash(repo: ?*git.git_repository) !void {
+    _ = git.git_stash_foreach(repo, stash_cb, null);
+    std.debug.print("Stashes: {}\n", .{COUNT});
+}
+
+fn stash_cb(index: usize, message: [*c]const u8, stash_id: [*c]const git.git_oid, payload: ?*anyopaque) callconv(.C) c_int {
+    _ = index;
+    _ = payload;
+    _ = stash_id;
+    _ = message;
+    COUNT += 1;
+    return 0;
+}
+
+fn status(repo: ?*git.git_repository) !void {
+    _ = git.git_status_foreach(repo, status_cb, null);
+}
+
+fn status_cb(path: [*c]const u8, status_flags: c_uint, payload: ?*anyopaque) callconv(.C) c_int {
+    _ = payload;
+    std.debug.print("{s}\n", .{path});
+    std.debug.print("{}\n", .{status_flags});
+    return 0;
+}
