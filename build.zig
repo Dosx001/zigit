e1f70886e823005adb2542dec95c67fc2c8fffae
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-04-26 17:36:21 -0700

    build: set optimization to fast

--- build.zig
+++ build.zig
@@ -4,10 +4,12 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "zigit",
         .root_source_file = .{ .path = "src/main.zig" },
+        .optimize = .ReleaseFast,
     });
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("git2");
     b.installArtifact(exe);
+    exe.optimize = .Debug;
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
