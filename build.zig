3940a6f77043667b87595aa3eaf9b52cbf01eb6e
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2024-05-11 11:32:36 -0700

    build: setup standardOptimizeOption

--- build.zig
+++ build.zig
@@ -4,12 +4,14 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "zigit",
         .root_source_file = .{ .path = "src/main.zig" },
-        .optimize = .ReleaseFast,
+        .target = b.standardTargetOptions(.{}),
+        .optimize = b.standardOptimizeOption(.{
+            .preferred_optimize_mode = .ReleaseFast,
+        }),
     });
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("git2");
     b.installArtifact(exe);
-    exe.optimize = .Debug;
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
     run_step.dependOn(&run_exe.step);
