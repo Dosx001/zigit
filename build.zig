b582472339553f9ed1e0df1f6e0778d40feaf1d3
Andres Rodriguez <andresmichelrodriguez@gmail.com>
2026-05-20 11:04:11 -0700

    fix: zig 0.16

--- build.zig
+++ build.zig
@@ -1,18 +1,28 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{
+        .preferred_optimize_mode = .ReleaseFast,
+    });
+    const translate_c = b.addTranslateC(.{
+        .root_source_file = b.path("src/c.h"),
+        .target = target,
+        .optimize = optimize,
+    });
+    translate_c.linkSystemLibrary("git2", .{});
     const exe = b.addExecutable(.{
         .name = "zigit",
         .root_module = b.createModule(.{
             .root_source_file = b.path("src/main.zig"),
-            .target = b.standardTargetOptions(.{}),
-            .optimize = b.standardOptimizeOption(.{
-                .preferred_optimize_mode = .ReleaseFast,
-            }),
+            .optimize = optimize,
+            .target = target,
+            .imports = &.{.{
+                .name = "c",
+                .module = translate_c.createModule(),
+            }},
         }),
     });
-    exe.linkLibC();
-    exe.linkSystemLibrary("git2");
     b.installArtifact(exe);
     const run_exe = b.addRunArtifact(exe);
     const run_step = b.step("run", "Run the application");
--- src/main.zig
+++ src/main.zig
@@ -1,30 +1,33 @@
+const c = @import("c");
 const std = @import("std");
-const git = @cImport(@cInclude("git2.h"));
 
-pub fn main() void {
-    _ = git.git_libgit2_init();
-    var repo: ?*git.git_repository = undefined;
-    if (git.git_repository_open_ext(
+pub fn main(init: std.process.Init) void {
+    const io = init.io;
+    _ = c.git_libgit2_init();
+    var repo: ?*c.git_repository = undefined;
+    if (c.git_repository_open_ext(
         &repo,
         ".",
         0,
         null,
-    ) == git.GIT_ENOTFOUND) return;
-    const path = git.git_repository_path(repo);
+    ) == c.GIT_ENOTFOUND) return;
+    const path = c.git_repository_path(repo);
     var buffer: [1024]u8 = undefined;
     log(repo);
-    status();
-    state(repo, path, &buffer);
-    branch(path, &buffer);
-    stash(path, &buffer);
+    status(io);
+    state(repo, path, &buffer, io);
+    branch(path, &buffer, io);
+    stash(path, &buffer, io);
     return;
 }
 
 fn branch(
     path: [*c]const u8,
     buffer: []u8,
+    io: std.Io,
 ) void {
-    const file = std.fs.openFileAbsolute(
+    const file = std.Io.Dir.openFileAbsolute(
+        io,
         std.fmt.bufPrint(
             buffer,
             "{s}HEAD",
@@ -32,7 +35,7 @@ fn branch(
         ) catch unreachable,
         .{},
     ) catch unreachable;
-    var reader = file.reader(buffer);
+    var reader = file.reader(io, buffer);
     const len = reader.interface.discardDelimiterLimit(
         '\n',
         std.Io.Limit.limited(32),
@@ -49,9 +52,9 @@ fn branch(
     );
 }
 
-fn log(repo: ?*git.git_repository) void {
-    var oid: git.git_oid = undefined;
-    if (git.git_reference_name_to_id(
+fn log(repo: ?*c.git_repository) void {
+    var oid: c.git_oid = undefined;
+    if (c.git_reference_name_to_id(
         &oid,
         repo,
         "HEAD",
@@ -59,19 +62,21 @@ fn log(repo: ?*git.git_repository) void {
         std.debug.print("\n", .{});
         return;
     }
-    var commit: ?*git.git_commit = undefined;
-    _ = git.git_commit_lookup(&commit, repo, &oid);
+    var commit: ?*c.git_commit = undefined;
+    _ = c.git_commit_lookup(&commit, repo, &oid);
     std.debug.print(
         "\x1b[90m{s}\n",
-        .{git.git_commit_summary(commit)},
+        .{c.git_commit_summary(commit)},
     );
 }
 
 fn stash(
     path: [*c]const u8,
     buffer: []u8,
+    io: std.Io,
 ) void {
-    const file = std.fs.openFileAbsolute(
+    const file = std.Io.Dir.openFileAbsolute(
+        io,
         std.fmt.bufPrint(
             buffer,
             "{s}logs/refs/stash",
@@ -87,7 +92,7 @@ fn stash(
             else => unreachable,
         }
     };
-    var reader = file.reader(buffer);
+    var reader = file.reader(io, buffer);
     var count: u8 = 0;
     while (reader.interface.takeDelimiterInclusive(
         '\n',
@@ -99,28 +104,30 @@ fn stash(
 }
 
 fn state(
-    repo: ?*git.git_repository,
+    repo: ?*c.git_repository,
     path: [*c]const u8,
     buffer: []u8,
+    io: std.Io,
 ) void {
-    const repo_state = git.git_repository_state(repo);
+    const repo_state = c.git_repository_state(repo);
     const mode =
         switch (repo_state) {
-            git.GIT_REPOSITORY_STATE_MERGE => "Merging onto",
-            git.GIT_REPOSITORY_STATE_REVERT => "Revert",
-            git.GIT_REPOSITORY_STATE_REVERT_SEQUENCE => "Revert",
-            git.GIT_REPOSITORY_STATE_CHERRYPICK => "Cherrypick",
-            git.GIT_REPOSITORY_STATE_CHERRYPICK_SEQUENCE => "Cherrypick",
-            git.GIT_REPOSITORY_STATE_BISECT => "Bisect",
-            git.GIT_REPOSITORY_STATE_REBASE => "Rebase",
-            git.GIT_REPOSITORY_STATE_REBASE_INTERACTIVE => "Rebase",
-            git.GIT_REPOSITORY_STATE_REBASE_MERGE => "Rebase/Merge",
-            git.GIT_REPOSITORY_STATE_APPLY_MAILBOX => "Mailbox",
-            git.GIT_REPOSITORY_STATE_APPLY_MAILBOX_OR_REBASE => "Mailbox or Rebase",
+            c.GIT_REPOSITORY_STATE_MERGE => "Merging onto",
+            c.GIT_REPOSITORY_STATE_REVERT => "Revert",
+            c.GIT_REPOSITORY_STATE_REVERT_SEQUENCE => "Revert",
+            c.GIT_REPOSITORY_STATE_CHERRYPICK => "Cherrypick",
+            c.GIT_REPOSITORY_STATE_CHERRYPICK_SEQUENCE => "Cherrypick",
+            c.GIT_REPOSITORY_STATE_BISECT => "Bisect",
+            c.GIT_REPOSITORY_STATE_REBASE => "Rebase",
+            c.GIT_REPOSITORY_STATE_REBASE_INTERACTIVE => "Rebase",
+            c.GIT_REPOSITORY_STATE_REBASE_MERGE => "Rebase/Merge",
+            c.GIT_REPOSITORY_STATE_APPLY_MAILBOX => "Mailbox",
+            c.GIT_REPOSITORY_STATE_APPLY_MAILBOX_OR_REBASE => "Mailbox or Rebase",
             else => return,
         };
-    if (repo_state == git.GIT_REPOSITORY_STATE_MERGE) {
-        const file = std.fs.openFileAbsolute(
+    if (repo_state == c.GIT_REPOSITORY_STATE_MERGE) {
+        const file = std.Io.Dir.openFileAbsolute(
+            io,
             std.fmt.bufPrint(
                 buffer,
                 "{s}MERGE_MSG",
@@ -128,7 +135,7 @@ fn state(
             ) catch unreachable,
             .{},
         ) catch unreachable;
-        var reader = file.reader(buffer);
+        var reader = file.reader(io, buffer);
         reader.interface.discardAll(14) catch unreachable;
         std.debug.print(
             "\x1b[30;41m {s} \x1b[42;31m",
@@ -143,33 +150,22 @@ fn state(
     );
 }
 
-fn status() void {
-    const args = [_][]const u8{ "git", "status", "-s" };
-    var child = std.process.Child.init(
-        &args,
+fn status(io: std.Io) void {
+    const result = std.process.run(
         std.heap.c_allocator,
-    );
-    child.stdout_behavior = .Pipe;
-    child.stderr_behavior = .Pipe;
-    _ = child.spawn() catch unreachable;
-    var stderr = std.ArrayListAlignedUnmanaged(u8, null){};
-    var stdout = std.ArrayListAlignedUnmanaged(u8, std.mem.Alignment.@"1").initCapacity(
-        std.heap.c_allocator,
-        1024,
-    ) catch unreachable;
-    _ = std.process.Child.collectOutput(
-        child,
-        std.heap.c_allocator,
-        &stdout,
-        &stderr,
-        1_048_576,
+        io,
+        .{ .argv = &.{
+            "git",
+            "status",
+            "-s",
+        } },
     ) catch unreachable;
     var j: usize = 0;
-    for (stdout.items, 0..) |c, i| {
-        if (c == '\n') {
-            const color = switch (stdout.items[j]) {
+    for (result.stdout, 0..) |char, i| {
+        if (char == '\n') {
+            const color = switch (result.stdout[j]) {
                 '?' => "38;2;204;204;204",
-                ' ' => switch (stdout.items[j + 1]) {
+                ' ' => switch (result.stdout[j + 1]) {
                     '?' => "38;2;204;204;204",
                     'm' => "38;2;255;255;0",
                     'M' => "38;2;193;156;0",
@@ -177,7 +173,7 @@ fn status() void {
                     'T' => "38;2;165;42;42",
                     else => "",
                 },
-                'A' => switch (stdout.items[j + 1]) {
+                'A' => switch (result.stdout[j + 1]) {
                     '?' => "38;2;204;204;204",
                     ' ' => "38;2;0;55;218",
                     'm' => "38;2;30;144;255",
@@ -188,33 +184,33 @@ fn status() void {
                     'T' => "38;2;95;158;168",
                     else => "",
                 },
-                'D' => switch (stdout.items[j + 1]) {
+                'D' => switch (result.stdout[j + 1]) {
                     ' ' => "38;2;231;72;86",
                     'D' => "38;2;0;0;0m\x1b[48;2;197;15;31",
                     'U' => "38;2;193;156;0m\x1b[48;2;197;15;31",
                     else => "",
                 },
-                'M' => switch (stdout.items[j + 1]) {
+                'M' => switch (result.stdout[j + 1]) {
                     ' ' => "38;2;19;161;14",
                     'D' => "38;2;255;95;0",
                     'M' => "38;2;249;241;165",
                     'T' => "38;2;244;164;96",
                     else => "",
                 },
-                'U' => switch (stdout.items[j + 1]) {
+                'U' => switch (result.stdout[j + 1]) {
                     'A' => "38;2;204;204;204m\x1b[48;2;0;55;218",
                     'D' => "38;2;204;204;204m\x1b[48;2;197;15;31",
                     'U' => "38;2;0;0;0m\x1b[48;2;193;156;0",
                     else => "",
                 },
-                'R' => switch (stdout.items[j + 1]) {
+                'R' => switch (result.stdout[j + 1]) {
                     ' ' => "38;2;136;23;152",
                     'D' => "38;2;255;0;255",
                     'M' => "38;2;135;0;255",
                     'T' => "38;2;238;130;238",
                     else => "",
                 },
-                'T' => switch (stdout.items[j + 1]) {
+                'T' => switch (result.stdout[j + 1]) {
                     ' ' => "38;2;210;105;30",
                     'D' => "38;2;240;128;128",
                     'M' => "38;2;255;215;0",
@@ -225,7 +221,7 @@ fn status() void {
             };
             std.debug.print(
                 "\x1b[{s}m{s}\x1b[0m ",
-                .{ color, stdout.items[j + 3 .. i] },
+                .{ color, result.stdout[j + 3 .. i] },
             );
             j = i + 1;
         }
